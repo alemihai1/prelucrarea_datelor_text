62ed31ae995c738e7931001370f67b897e041db8
alemihai1 <73282996+alemihai1@users.noreply.github.com>
2021-01-22 19:07:25 +0200

    Add files via upload

---
+++ pcgarage.py
@@ -0,0 +1,101 @@
+################## WEB SCRAPING ##################
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as soup
+import urllib.request
+import csv
+req = urllib.request.Request("https://www.pcgarage.ro/audio-foto-video/", headers={"User-Agent": "Chrome"})
+uclient = ureq(req)
+page_html1 = uclient.read()
+uclient.close()
+page_soup1 = soup(page_html1, "html.parser")
+subcategorii = page_soup1.findAll("small")
+for subcat in subcategorii:
+    print(subcat)
+
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as soup
+import urllib.request
+import csv
+
+filename = "pcgarage.csv"
+with open(filename, 'w', newline='', encoding="utf-16") as csvfile:
+    fieldnames = ['nume', 'product', 'rating', 'pro', 'contra', 'altele']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter = "\t")
+    writer.writeheader()
+
+    req = urllib.request.Request("https://www.pcgarage.ro/", headers={"User-Agent": "Chrome"})
+    uclient = ureq(req)
+    page_html1 = uclient.read()
+    uclient.close()
+    page_soup1 = soup(page_html1, "html.parser")
+
+    categorii = page_soup1.findAll("a", {"class": "cat-nav-tab"})
+
+    for categ in categorii:
+        if categ:
+            req = urllib.request.Request(categ['href'], headers={"User-Agent": "Chrome"})
+        uclient = ureq(req)
+        page_html2 = uclient.read()
+        uclient.close()
+        page_soup2 = soup(page_html2, "html.parser")
+
+        subcategorii = page_soup2.findAll("small")
+
+        for subcat in subcategorii:
+            try:
+                req = urllib.request.Request(subcat.a['href'], headers={"User-Agent": "Chrome"})
+            except:
+                continue
+            uclient = ureq(req)
+            page_html3 = uclient.read()
+            uclient.close()
+            page_soup3 = soup(page_html3, "html.parser")
+
+            containers = page_soup3.findAll("div", {"class": "product_box_container"})
+            ratingbox = page_soup3.findAll("div", {"class": "product_box_rating"})
+            for container in ratingbox:
+                try:
+                    req = urllib.request.Request(container.a['href'], headers={"User-Agent": "Chrome"})
+                except:
+                    continue
+                uclient = ureq(req)
+                page_html4 = uclient.read()
+                uclient.close()
+                page_soup4 = soup(page_html4, "html.parser")
+
+                comment_page = page_soup4.findAll("p", {"class": "ar_see_all clearfix"})
+                try:
+                    req = urllib.request.Request(comment_page[0].a['href'], headers={"User-Agent": "Chrome"})
+                except:
+                    continue
+                uclient = ureq(req)
+                page_html5 = uclient.read()
+                uclient.close()
+
+                page_soup5 = soup(page_html5, "html.parser")
+                product_name = page_soup5.findAll("h1", {"id": "product_name"})
+                reviews = page_soup5.findAll("div", {"itemprop": "review"})
+
+                for review in reviews:
+                    row = {}
+                    name = review.findAll("span", {"itemprop": "author"})
+                    name = name[0].text.strip()
+                    rating = review.findAll("span", {"itemprop": "ratingValue"})
+                    rating = rating[0].text.strip()
+                    comments = review.findAll("p", {"itemprop": "description"})
+                    row['nume'] = name
+                    row['rating'] = rating
+                    row['product'] = product_name[0].span.text
+                    row['pro'] = "-"
+                    row['contra'] = "-"
+                    row['altele'] = "-"
+                    for comment in comments:
+                        cat, *_ = comment.findAll("b")
+                        if cat.text.strip() == 'Pro:' :
+                            row['pro'] = (comment.text.strip()).replace("\n", " ").strip()
+                        elif cat.text.strip() == 'Contra:' :
+                            row['contra'] = (comment.text.strip()).replace("\n", " ").strip()
+                        else:
+                            row['altele'] = (comment.text.strip()).replace("\n", " ").strip()
+                    print(row)
+                    writer.writerow(row)
